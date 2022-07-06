af8fea325dae9963d4c7ac776cbcb4c3411dc75a
cataelisa <catalinamoralesmedina@gmail.com>
2022-07-05 22:27:07 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function goToSecondPage(){
+    //Get the card number and keep it
+    const numberHtmlElement = document.getElementById ('card-number');
+    const creditCardNumber = numberHtmlElement.value;
+
+    //Ask users to enter a card number in case they did not
+    if (creditCardNumber=='') {
+    alert('Debes ingresar el número de tu tarjeta de crédito');
+    }
+
+      // Hide the first page
+    const firstPage = document.getElementById('first-page');
+    firstPage.style.display = 'none'
+
+    // Show the second page
+    const secondPage = document.getElementById('second-page');
+    secondPage.style.display = 'block'
+}
+
+
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
 const validator = {
-  // ...
+  // To get the credit card number (number string) and validate it
+
+
+  // To hide the first 12 credit card numbers//
+
 };
 
 export default validator;
