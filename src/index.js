3605e4038e5974a87428bcece5288bbf342a9d5d
cataelisa <catalinamoralesmedina@gmail.com>
2022-07-13 12:03:46 -0400

    third

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-function goToSecondPage(){
+
+import validator from './validator.js';
+
+function showNumber(cardNumber){
+    // show card number on divs
+    let hiddenNumberHtmlElement = document.getElementById ('hidden-card-number');
+    hiddenNumberHtmlElement.innerHTML = validator.maskify(cardNumber)
+    }
+
+function goToSecondPage(){ 
+  
     //Get the card number and keep it
-    const numberHtmlElement = document.getElementById ('card-number');
-    const creditCardNumber = numberHtmlElement.value;
+    let numberHtmlElement = document.getElementById ('card-number');
+    let creditCardNumber = numberHtmlElement.value;
 
     //Ask users to enter a card number in case they did not
-    if (creditCardNumber=='') {
-    alert('Debes ingresar el número de tu tarjeta de crédito');
+    if (creditCardNumber == '') {
+    alert ('Debes ingresar el número de tu tarjeta de crédito');
+    return
     }
 
       // Hide the first page
-    const firstPage = document.getElementById('first-page');
+    let firstPage = document.getElementById('first-page');
     firstPage.style.display = 'none'
 
     // Show the second page
-    const secondPage = document.getElementById('second-page');
+    let secondPage = document.getElementById('second-page');
     secondPage.style.display = 'block'
+
+    showNumber(creditCardNumber)
 }
+//ONCLICK
+let buttonValidate = document.getElementById('button');
+console.log(buttonValidate)
 
-
-import validator from './validator.js';
+buttonValidate.addEventListener('click', goToSecondPage);
 
 console.log(validator);
 
--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,19 @@
 const validator = {
   // To get the credit card number (number string) and validate it
 
-
   // To hide the first 12 credit card numbers//
+  maskify:  function (creditCardNumber){
+    console.log (creditCardNumber)
+    return creditCardNumber.split('').map((letter, idx) => idx < creditCardNumber.lenght - 4 ? '#' : letter).join('');
+    }
 
 };
 
+
+
+
+/*function maskify (creditCardNumber){
+return creditCardNumber.split('').map((letter, idx) => idx < creditCardNumber.lenght - 4 ? '#': letter).join('');
+} */
+ 
 export default validator;
